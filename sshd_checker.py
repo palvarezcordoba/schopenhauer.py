11a2dc74f560deadd2eca148fad06f50cf0c1b98
David Roman <davidroman96@gmail.com>
2017-07-07 14:42:28 +0000

    Add logger

--- sshd_checker.py
+++ sshd_checker.py
@@ -1,54 +1,89 @@
 #!/usr/bin/env python3
 
-import sshd_parser
 import re
+import logging
 from os import popen
 from random import randint
 from sys import stderr
 
-with popen("/usr/sbin/sshd -T") as p:
-    sshd_config = p.read()
-sshd = sshd_parser.SSHConf(sshd_config)
-
-def check_root():
-    if sshd["permitrootlogin"] in ("yes", "prohibit-password", "without-password"):
-        print("You should not use root account.", "Set PermitRootLogin to no please.", sep=' ')
-
-def check_port():
-    if sshd['port'] == '22':
-        print("Port number should not be the default (22)", "Set Port to other please. For example: {}".format(randint(5000, 65535), sep=' '))
-
-def check_logingracetime():
-    if int(sshd["logingracetime"]) > 25:
-        print("LoginGraceTime is very high, if you do not need so much, reduce the value of this variable.")
-
-def check_passauthentication():
-    if sshd["passwordauthentication"] == 'yes' or sshd["challengeresponseauthentication"] == 'yes':
-        print("You should disable keyboard-interactive and use ssh keys instead (or convine it, for example ssh keys + OTP codes). Make sure than PasswordAuthentication and ChallengeResponseAuthentication is both disabled.")
-
-def check_2fa():
-    with open("/etc/pam.d/sshd", 'r') as f:
-        sshd_pam = f.read()
-    if re.match("\s*auth\s*required\s*pam_google_authenticator.so*", sshd_pam):
-        print("It is recommended use 2FA.")
-        return 0
-    opt_or_suff = re.match("\s*auth\s*(optional|sufficient)\s*pam_google_authenticator.so*", sshd_pam)
-    if opt_or_suff is not None:
-        print("You should not use {} option in /etc/pam.d/sshd.".format(opt_or_suff.group()))
-
-def login_filter():
-    if (not "allowusers" in sshd.getOptions()) or (not "allowgroups" in sshd.getOptions()):
-        print("Filter users/groups with AllowUSers, AllowGroups.")
-
-def check_privilege_separation():
-    if sshd["useprivilegeseparation"] != "sandbox":
-        print("Please, use \"UsePrivilegeSeperation sandbox\" option.")
-
-def check_subsystem():
-    if "subsystem" in sshd.getOptions():
-        print("If you not really need {} disable it.".format(sshd["subsystem"]))
+logging.basicConfig(level=logging.DEBUG,
+                    format="[%(levelname)s::%(name)s] %(message)s")
 
 
-__all__ = [check_root, check_port, check_logingracetime, check_passauthentication, check_2fa, login_filter, check_privilege_separation, check_subsystem]
+class SSHConf:
 
-[ x() for x in __all__]
+    def __init__(self, conf):
+        self.conf = [x.split(' ', 1) for x in conf.rsplit('\n')]
+        self.confdict = dict()
+        for x in self.conf:
+            try:
+                if x[0] in self.confdict.keys():
+                    self.confdict[x[0]] = (self.confdict[x[0]], x[1])
+                else:
+                    self.confdict.update({x[0]: x[1]})
+            except:
+                pass
+
+    def __getitem__(self, x):
+        return self.confdict[x]
+
+    def getOptions(self):
+        return self.confdict.keys()
+
+
+class SSHCheck:
+
+    def __init__(self):
+        with popen("/usr/sbin/sshd -T") as p:
+            sshd_config = p.read()
+        self._sshd = SSHConf(sshd_config)
+        self._log = logging.getLogger(self.__class__.__name__)
+
+    def root(self):
+        if self._sshd["permitrootlogin"] != "no":
+            self._log.error(
+                "You should not use root account. Set PermitRootLogin to no please.")
+
+    def port(self):
+        if self._sshd['port'] == '22':
+            self._log.error("Port number should not be the default (22). Set Port to other please. For example: {}".format(
+                randint(5000, 65535)))
+
+    def logingracetime(self):
+        if int(self._sshd["logingracetime"]) > 25:
+            self._log.error(
+                "LoginGraceTime is very high, if you do not need so much, reduce the value of this variable.")
+
+    def passauthentication(self):
+        if self._sshd["passwordauthentication"] == 'yes' or self._sshd["challengeresponseauthentication"] == 'yes':
+            self._log.error("You should disable keyboard-interactive and use ssh keys instead (or convine it, for example ssh keys + OTP codes). Make sure than PasswordAuthentication and ChallengeResponseAuthentication is both disabled.")
+
+    def TFA(self):
+        with open("/etc/pam.d/sshd", 'r') as f:
+            sshd_pam = f.read()
+        if re.match("\s*auth\s*required\s*pam_google_authenticator.so*", sshd_pam):
+            self._log.error("It is recommended use 2FA.")
+            return 0
+        opt_or_suff = re.match(
+            "\s*auth\s*(optional|sufficient)\s*pam_google_authenticator.so*", sshd_pam)
+        if opt_or_suff is not None:
+            self._log.error(
+                "You should not use {} option in /etc/pam.d/sshd.".format(opt_or_suff.group()))
+
+    def login_filter(self):
+        if (not "allowusers" in self._sshd.getOptions()) or (not "allowgroups" in self._sshd.getOptions()):
+            self._log.error(
+                "Filter users/groups with AllowUSers, AllowGroups.")
+
+    def subsystem(self):
+        if "subsystem" in self._sshd.getOptions():
+            self._log.error("If you not really need {} disable it.".format(
+                self._sshd["subsystem"]))
+
+
+checker = SSHCheck()
+__all__ = [checker.root, checker.port, checker.logingracetime,
+           checker.passauthentication, checker.TFA, checker.login_filter,
+           checker.subsystem]
+
+[x() for x in __all__]
