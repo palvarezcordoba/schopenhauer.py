6c277659e88dd466d64c9b119c63bbdf16adedb9
Pajaro_xdd <pabloalvarezcordoba@protonmail.ch>
2017-07-24 01:04:15 +0200

    tmp modified and added more checkers.

--- mounts_checker.py
+++ mounts_checker.py
@@ -17,19 +17,41 @@ class MountCheck:
     def tmp(self):
         for i in self._partitions:
             if i[1] == "/tmp":
-                if not "noexec" in i[3]:
-                    log.error("/tmp mountpoint should have *noexec* option")
+                if not "noexec" in i[3] or "nosuid" not in i[3] \
+                        or "nodev" not in i[3]:
+                    log.error("/tmp mountpoint should have noexec, nosuid and"
+                              " nodev options.")
                     return
- 
-        log.error("/tmp should be separated and have *noexec* option")
+        log.error(
+            "/tmp should be separated and have noexec, nosuid and nodev options.")
+
+    def home(self):
+        for i in self._partitions:
+            if i[1] == "/home":
+                return
+        log.error("/home should be separated.")
+
+    def tmpfs(self):
+        for i in self._partitions:
+            if i[0] == "tmpfs":
+                if "nosuid" not in i[3] or "nodev" not in i[3]:
+                    log.error(
+                        "{} should have nosuid and nodev options.".format(i[1]))
+
+    def usage(self):
+        for partition in psutil.disk_partitions():
+            usage = psutil.disk_usage(partition[1])[-1]
+            if usage >= 90:
+                log.error("Usage of {}: {}%".format(partition[1], usage))
 
 
 def run():
     checker = MountCheck()
-    
+
     c = helpers.getCheckers(MountCheck, CHECKER_NAME)
     for name in sorted(c):
         getattr(checker, name)()
 
+
 if __name__ == "__main__":
-	run()
+    run()
