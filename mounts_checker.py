ff42456182d866fd338eaaf00b90ad6169c91afe
Pajaro_xdd <pabloalvarezcordoba@protonmail.ch>
2017-07-24 00:12:31 +0200

    Minor changes.

--- mounts_checker.py
+++ mounts_checker.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 
 import logging
-
 import psutil
-
 import helpers
 
-CHECKER_NAME = "MOUNT"
 
+CHECKER_NAME = "MOUNT"
 logging.basicConfig(format="[%(name)s] %(message)s")
 log = logging.getLogger(CHECKER_NAME)
 
 
-class MountCheck(object):
-
+class MountCheck:
     def __init__(self):
         self._partitions = psutil.disk_partitions(all=True)
 
@@ -26,13 +23,13 @@ class MountCheck(object):
  
         log.error("/tmp should be separated and have *noexec* option")
 
+
 def run():
     checker = MountCheck()
     
     c = helpers.getCheckers(MountCheck, CHECKER_NAME)
     for name in sorted(c):
         getattr(checker, name)()
-
 
 if __name__ == "__main__":
 	run()
