ddb11eb96c8a4eddbb2a12b39b50a17ac71e95d9
Pajaro_xdd <pabloalvarezcordoba@protonmail.ch>
2017-07-26 07:54:32 +0200

    sysctl class

---
+++ sysctl.py
@@ -0,0 +1,10 @@
+
+class sysctl:
+	def read(self, key):
+		try:
+			f = open("/proc/sys/%s" % key.replace(".", "/"))
+		except:
+			return None
+		value = f.readline().strip()
+		f.close()
+		return value
