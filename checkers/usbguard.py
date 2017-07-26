08be17c0847f660b6fba16f0e54edf62f837f5f0
Pajaro_xdd <pabloalvarezcordoba@protonmail.ch>
2017-07-26 08:40:00 +0200

    Add usbguard checker

---
+++ checkers/usbguard.py
@@ -0,0 +1,27 @@
+import logging
+import os
+import helpers
+
+CHECKER_NAME = "USBGUARD"
+
+logging.basicConfig(format="[%(name)s] %(message)s")
+log = logging.getLogger(CHECKER_NAME)
+
+
+class USBGuard:
+	def checkUSBGuardIsInstalled(self):
+		try:
+			os.stat("/usr/bin/usbguard")
+		except FileNotFoundError:
+			log.error("usbguard is not installed.")
+
+
+def run():
+    checker = USBGuard()
+    
+    c = helpers.getCheckers(USBGuard, CHECKER_NAME)
+    for name in sorted(c):
+        getattr(checker, name)()
+
+if __name__ == "__main__":
+    run()
