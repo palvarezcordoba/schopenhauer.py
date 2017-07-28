0f4812c8f659ce10d1aca159de0b8e978df5f9f9
David Roman <davidroman96@gmail.com>
2017-07-28 17:50:51 +0000

    Execute module depending on argv[0]

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ logging.basicConfig(format="[%(name)s] %(message)s")
 log = logging.getLogger(__name__)
 
 sys.path.append("checkers")
-checkers = []
-for m in os.scandir("checkers"):
-    if m.is_file() and m.name.endswith(".py"):
-        checkers.append(__import__(m.name[:-3]))
-for c in checkers:
-    try:
-        if c.makes_sense():
-            c.run()
-    except Exception as e:
-        traceback.print_exc()
-        log.error(e)
-        break
+
+name = sys.argv[0].replace(".", " ").split()
+if len(name) > 1 and  name[1] != "py":
+    c = __import__(name[1])
+    if c.makes_sense():
+        c.run()
+else:
+    checkers = []
+    for m in os.scandir("checkers"):
+        if m.is_file() and m.name.endswith(".py"):
+            checkers.append(__import__(m.name[:-3]))
+    for c in checkers:
+        try:
+            if c.makes_sense():
+                c.run()
+        except Exception as e:
+            traceback.print_exc()
+            log.error(e)
+            break
